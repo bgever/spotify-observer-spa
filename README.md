8674ec80baffa270a70314839b18599b7a3d1687
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2019-12-14 21:56:31 +0800

    initial project: preact + typescript + parcel

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spotify-observer-spa
 Single Page App of the Spotify Observer project
+
+Helpful reading:
+ - https://denys.dev/preact-project-with-typescript-and-parcel/
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { h, render } from 'preact';
+import './styles.scss';
+
+const App = () => <h1>Spotify Observer</h1>;
+
+render(<App />, document.getElementById('root'));
